cb9bfdd7bb30ee0d20cdb53e4e62989fcc77503a
youpiteron <oyushchenko@kse.org.ua>
2023-09-06 11:21:07 +0300

    First actual commit(doesn't work)

--- STE/Source.c
+++ STE/Source.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
 int main() {
-	printf("Hello World!");
+	char text[80];
+	int command;
+	while (&command != "9")
+	{
+		printf("Choose the command:\n");
+		scanf_s("%i", &command);
+		switch (command)
+		{
+		case 1:
+			printf("\nEnter text to append:");
+			scanf_s("%s", text, sizeof(text));
+			break;
+		case 2:
+			text[strlen(text)] = "\n";
+			printf("New line is started");
+			break;
+		}
+		for (int i = 0; i < strlen(text); i++)
+			printf("%c", text[i]);
+	}
 	return 0;
 }

72f20397c4cc1741ed7f1ba2ddb5aa2b0fd6168e
youpiteron <oyushchenko@kse.org.ua>
2023-09-20 11:23:09 +0300

    doesnt work writing:(

--- STE/Source.c
+++ STE/Source.c
@@ -1,25 +1,76 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+	char symbol;
+	struct node* next;
+};
 
 int main() {
+	struct node *head = (struct node*)malloc(sizeof(struct node));
+	struct node* pointer = NULL;
+	head->next = NULL;
 	char text[80];
+	FILE** myFile;
 	int command;
 	while (&command != "9")
 	{
 		printf("Choose the command:\n");
+		
 		scanf_s("%i", &command);
 		switch (command)
 		{
+		case 0:
+			pointer = head;
+			while (pointer->next != NULL) {
+				printf("%c", pointer->next->symbol);
+				pointer = pointer->next;
+			}
+			printf("\n");
+			break;
 		case 1:
-			printf("\nEnter text to append:");
+			printf("Enter text to append:");
 			scanf_s("%s", text, sizeof(text));
+			int i = 0;
+			pointer = head;
+			while (pointer->next != NULL) {
+				pointer = pointer->next;
+			}
+			while (text[i] != '\0') {
+				struct node *newnode = (struct node*)malloc(sizeof(struct node));
+				newnode->symbol = text[i];
+				newnode->next = NULL;
+				pointer->next = newnode;
+				pointer = pointer->next;
+				i++;
+			}
 			break;
 		case 2:
-			text[strlen(text)] = "\n";
-			printf("New line is started");
+			pointer = head;
+			while (pointer->next != NULL) {
+				pointer = pointer->next;
+			}
+			struct node* newnode = (struct node*)malloc(sizeof(struct node));
+			newnode->symbol = '\n';
+			newnode->next = NULL;
+			pointer->next = newnode;
+			printf("New line is started\n");
 			break;
+		case 3:
+			printf("Enter file name for saving:");
+			char filename[20];
+			scanf_s("%s", filename, sizeof(filename));
+			fopen_s(myFile, filename, "w");
+			pointer = head;
+			while (pointer->next != NULL) {
+				fprintf(myFile, pointer->symbol);
+				pointer = pointer->next;
+			}
+			head = NULL;
+			head = (struct node*)malloc(sizeof(struct node));
+			pointer = NULL;
 		}
-		for (int i = 0; i < strlen(text); i++)
-			printf("%c", text[i]);
+		
 	}
 	return 0;
 }

49082765e62f49eb52cd2c07143cf95641788582
youpiteron <oyushchenko@kse.org.ua>
2023-09-22 22:28:41 +0300

    File reading and writing works(also doesnt work insert)

--- STE/Source.c
+++ STE/Source.c
@@ -11,27 +11,21 @@ int main() {
 	struct node* pointer = NULL;
 	head->next = NULL;
 	char text[80];
-	FILE** myFile;
-	int command;
-	while (&command != "9")
+	FILE* myFile;
+	char filename[20];
+	int command = 0;
+	int i;
+	while (command != 9)
 	{
 		printf("Choose the command:\n");
 		
 		scanf_s("%i", &command);
 		switch (command)
 		{
-		case 0:
-			pointer = head;
-			while (pointer->next != NULL) {
-				printf("%c", pointer->next->symbol);
-				pointer = pointer->next;
-			}
-			printf("\n");
-			break;
 		case 1:
 			printf("Enter text to append:");
 			scanf_s("%s", text, sizeof(text));
-			int i = 0;
+			i = 0;
 			pointer = head;
 			while (pointer->next != NULL) {
 				pointer = pointer->next;
@@ -58,17 +52,74 @@ int main() {
 			break;
 		case 3:
 			printf("Enter file name for saving:");
-			char filename[20];
+			
 			scanf_s("%s", filename, sizeof(filename));
-			fopen_s(myFile, filename, "w");
-			pointer = head;
-			while (pointer->next != NULL) {
-				fprintf(myFile, pointer->symbol);
+			fopen_s(&myFile, filename, "w");
+			pointer = head->next;
+			while (pointer != NULL) {
+				fprintf(myFile, "%c", pointer->symbol);
 				pointer = pointer->next;
 			}
+			fflush(myFile);
+			fclose(myFile);
 			head = NULL;
 			head = (struct node*)malloc(sizeof(struct node));
+			head->next = NULL;
 			pointer = NULL;
+			break;
+		case 4:
+			printf("Enter file name for reading:");
+			scanf_s("%s", filename, sizeof(filename));
+			fopen_s(&myFile, filename, "r");
+			char symbol;
+			pointer = head;
+			while (pointer->next != NULL) {
+				pointer = pointer->next;
+			}
+			while ((symbol = fgetc(myFile)) != EOF) {
+				struct node* newnode = (struct node*)malloc(sizeof(struct node));
+				newnode->symbol = symbol;
+				newnode->next = NULL;
+				pointer->next = newnode;
+				pointer = pointer->next;
+			}
+			fclose(myFile);
+			break;
+		case 5:
+			pointer = head;
+			while (pointer->next != NULL) {
+				printf("%c", pointer->next->symbol);
+				pointer = pointer->next;
+			}
+			printf("\n");
+			break;
+		case 6:;
+			int line = 0;
+			int sym = 0;
+			printf("Choose line and index:");
+			scanf_s("%i%i", line, sym);
+			pointer = head;
+			while (line > 0 && sym > 0) {
+				if (pointer->symbol == "\n") {
+					line--;
+				}
+				if (line == 0) {
+					sym--;
+				}
+				pointer = pointer->next;
+			}
+
+			printf("Enter text to append:");
+			scanf_s("%s", text, sizeof(text));
+			i = 0;
+			while (text[i] != '\0') {
+				struct node* newnode = (struct node*)malloc(sizeof(struct node));
+				newnode->symbol = text[i];
+				newnode->next = NULL;
+				pointer->next = newnode;
+				pointer = pointer->next;
+				i++;
+			}
 		}
 		
 	}
